8b665a0209323120b5e68fe98534d002a2a18de2
Dawid <20098868@mail.wit.ie>
2023-10-20 13:15:43 +0200

    Added filtering of movie list.

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -39,8 +39,9 @@ export default function FilterMoviesCard(props) {
 
   const handleChange = (e, type, value) => {
     e.preventDefault()
+    props.onUserInput(type, value)
     // Completed later
-  };
+  }
   const handleTextChange = e => {
     handleChange(e, "name", e.target.value)
   }
