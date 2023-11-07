08d35e829c2ae73f285be6dce33d88732396032a
Dawid <20098868@mail.wit.ie>
2023-11-07 17:16:37 +0100

    Added routing and live data from API.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
-import { getMovies } from "../api/tmdb-api";
+
 
 const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
@@ -15,9 +15,17 @@ const HomePage = (props) => {
   };
 
   useEffect(() => {
-    getMovies().then(movies => {
-      setMovies(movies);
-    });
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+         console.log(json);
+        return json.results;
+      })
+      .then((movies) => {
+        setMovies(movies);
+      });
   }, []);
 
   return (
