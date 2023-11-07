6c278cb68d39ef8e242adec344a4142608a128b9
Dawid <20098868@mail.wit.ie>
2023-11-07 18:53:47 +0100

    Refactor: Template list page; HTTP API calls centralized.

--- movies/src/pages/favoriteMoviesPage.js
+++ movies/src/pages/favoriteMoviesPage.js
@@ -1,8 +1,18 @@
 import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
 
-const FavoriteMoviesPage = () => {
+const FavoriteMoviesPage = (props) => {
+  const toDo = () => true;
   // Get movies from local storage.
-  return <h2>Favorite Movies</h2>
-}
+  const movies = JSON.parse(localStorage.getItem("favorites")); 
 
-export default FavoriteMoviesPage
+  return (
+    <PageTemplate
+      title="Favourite Movies"
+      movies={movies}
+      selectFavorite={toDo}
+    />
+  );
+};
+
+export default FavoriteMoviesPage;
